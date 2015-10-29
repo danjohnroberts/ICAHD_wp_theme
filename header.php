fa5b79d86b62b785ac3f509ee3ccf944222b3918
Noel Light-Hilary <noel@noellh.com>
2015-10-29 19:20:29 +0000

    Remove junk compatibility warning JS files, see #10

--- header.php
+++ header.php
@@ -24,10 +24,6 @@
 <?php wp_head(); ?>
 <!-- end of wordpress head -->
 
-<!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-<script src="<?php bloginfo('template_directory');?>/library/js/ie-emulation-modes-warning.js"></script>
-
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
